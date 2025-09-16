3d8a6b420aa4fb23e5ea4de748d2ae604fb5639a
CalebSage <ehodges001@student.butte.edu>
2025-09-16 17:21:56 +0000

    ver2

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,10 @@
-
+print("hello world")
+A = "hello" 
+B = "world"
+print(A,B)
+this_number = 42
+this_number + 5
+print(this_number + 5)
+this_number = "17"
+this_number = int(this_number) + 5
+print(this_number)
